b3491d404a7ba836340e7a76c463fd04330ae1f9
Shampad Sharkar <sompodsign@gmail.com>
2021-03-10 10:01:46 +0600

    change style of input field and output fields as well. 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,24 @@ import Header from "./Header"
 
 function App(){
 
-    const [newText, setText] = useState("Text will appear here.")
+    const [newText, setText] = useState("");
+    const [words, setwords] = useState(0);
 
     function handleChange(event){
-        const newValue = event.target.value
-        setText(newValue)
+        const newValue = event.target.value;
+        setText(newValue ? newValue : "Text will appear here")
+        const newWords = newText.split().length;
+        setwords(newWords)
     }
-
+    function handleClick(){
+        setText("Text will appear here")
+    }
+console.log(words)
     return <div className="container">
         <Header />
         <h2>{newText}</h2>
-        <input onChange={handleChange} type="textarea"  placeholder="Start typing"/>
-        <button>RESET</button>
+        <input onChange={handleChange} type="textarea" value={newText === "Text will appear here" ? "" : newText} placeholder="Start typing"/>
+        <button onClick={handleClick}>RESET</button>
     </div>
 }
 export default App;
